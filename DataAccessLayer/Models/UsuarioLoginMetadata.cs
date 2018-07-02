b50538acdd4e1c30192c6c6488c1518493b1e77f
pablee <tcpipablo@gmail.com>
2018-07-02 12:01:00 -0300

    Login y registro agregado
    -Falta lo de comentarios y archivos.
    -Checkbox que no funciona.
    -Mejorar la vista.

---
+++ DataAccessLayer/Models/UsuarioLoginMetadata.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    class UsuarioLoginMetadata
+    {
+        [Required]
+        [StringLength(50)]
+        [EmailAddress(ErrorMessage = "El email ingresado es invalido")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Contrasenia { get; set; }
+    }
+}
--- WebBoxToDo/Controllers/HomeController.cs
+++ WebBoxToDo/Controllers/HomeController.cs
@@ -4,26 +4,184 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DataAccessLayer;
+using DataAccessLayer.Models;
+using Newtonsoft.Json.Linq;
+using System.Data.Entity.Validation;
+using System.Net;
+
 
 namespace WebBoxToDo.Controllers
 {
     public class HomeController : Controller
     {
+        BoxToDo_Contexto ctx = new BoxToDo_Contexto();
+
         public ActionResult Index()
         {
+            if (Request.Cookies["recordarme"] != null && Request.Cookies["recordarme"]["estado"] == "verdadero")
+            {
+                int id = Convert.ToInt32(Request.Cookies["recordarme"]["idUsuario"]);
+                Usuario UsLog = ctx.Usuario.FirstOrDefault(o => o.IdUsuario == id);
+                Session["login"] = true;
+                Session["id"] = UsLog.IdUsuario;
+
+                return View("Home", UsLog);
+            }
+
             return View();
         }
 
+        //Devuelve la vista.
         public ActionResult Registracion()
         {
             return View();
         }
 
+        //Recibe el formulario de registro.
         [HttpPost]
-        public ActionResult Login(string email, string contrasenia)
+        public ActionResult Registracion(UsuarioRegistro UsReg)
         {
+            var response = Request["g-recaptcha-response"];
+            string secretKey = "6Ld3NWAUAAAAAJJ2Mco-UNBPCdXwIZwIeNs1r6fC";
+            var client = new WebClient();
+            var result = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secretKey, response));
+            var obj = JObject.Parse(result);
+            var status = (bool)obj.SelectToken("success");
+            ViewBag.Message = status ? "Google reCaptcha validation success" : "Google reCaptcha validation failed";
+            status = true;
 
+            if (ModelState.IsValid && status == true)
+            {
+                BoxToDo_Contexto ctx = new BoxToDo_Contexto();
+                Usuario UsEncontrado = ctx.Usuario.FirstOrDefault(o => o.Email == UsReg.Email && o.Activo == 0);
+
+                if (UsEncontrado == null)
+                {
+                    Usuario UsNuevo = new Usuario();
+                    UsNuevo.Nombre = UsReg.Nombre;
+                    UsNuevo.Apellido = UsReg.Apellido;
+                    UsNuevo.Email = UsReg.Email;
+                    UsNuevo.Contrasenia = UsReg.Contrasenia;
+                    UsNuevo.FechaRegistracion = DateTime.Now;
+                    UsNuevo.CodigoActivacion = result;
+
+                    ctx.Usuario.Add(UsNuevo);
+                    //ctx.SaveChanges();
+
+                    try
+                    {
+                        ctx.SaveChanges();
+                    }
+                    catch (DbEntityValidationException ex)
+                    {
+                        // Retrieve the error messages as a list of strings.
+                        var errorMessages = ex.EntityValidationErrors
+                                .SelectMany(x => x.ValidationErrors)
+                                .Select(x => x.ErrorMessage);
+
+                        // Join the list to a single string.
+                        var fullErrorMessage = string.Join("; ", errorMessages);
+
+                        // Combine the original exception message with the new one.
+                        var exceptionMessage = string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
+
+                        // Throw a new DbEntityValidationException with the improved exception message.
+                        throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
+                    }
+                }
+                else
+                {
+                    UsEncontrado.Nombre = UsReg.Nombre;
+                    UsEncontrado.Apellido = UsReg.Apellido;
+                    UsEncontrado.Contrasenia = UsReg.Contrasenia;
+                    UsEncontrado.Activo = 1;
+                    UsEncontrado.FechaActivacion = DateTime.Now;
+
+                    ViewBag.Reactivado = UsReg;
+                    return View("Reactivacion");
+                }
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(UsReg);
+            }
+
+        }
+
+
+        public ActionResult Home()
+        {
+            if (Session["login"] is true)
+            {
+                return View();
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+
+        //Devuelve la vista.
+        public ActionResult Login()
+        {
             return View();
+        }
+
+        //Recibe el formulario de login.
+        [HttpPost]
+        public ActionResult Login(UsuarioLogin Us)
+        {
+            Usuario UsLog = ctx.Usuario.FirstOrDefault(o => o.Email == Us.Email);
+
+            if (ModelState.IsValid && UsLog != null)
+            {
+                if (UsLog.Activo == 1)
+                {
+                    if ((UsLog.Email == Us.Email) && (UsLog.Contrasenia == Us.Contrasenia))
+                    {
+                        if (Us.Recordarme is true)
+                        {
+                            HttpCookie recordarme = new HttpCookie("recordarme");
+                            recordarme["estado"] = "verdadero";
+                            recordarme["idUsuario"] = UsLog.IdUsuario.ToString();
+                            recordarme.Expires = DateTime.Now.AddDays(1);
+                            Response.Cookies.Add(recordarme);
+                        }
+
+                        Session["login"] = true;
+                        Session["id"] = UsLog.IdUsuario;
+                        return View("Home", UsLog);
+                    }
+                    else
+                    {
+                        ViewData["Invalido"] = "Verifique usuario y / o contraseña.";
+                    }
+                }
+                else if (UsLog.Activo == 0)
+                {
+                    ViewData["Inactivo"] = "Usuario inactivo.";
+                }
+            }
+            else
+            {
+                ViewData["Invalido"] = "Verifique usuario y / o contraseña.";
+            }
+
+            return View("Login", Us);
+        }
+
+        //Finaliza la sesion del usuario.
+        public ActionResult Logout()
+        {
+            if (Request.Cookies["recordarme"] != null)
+            {
+                Response.Cookies["recordarme"].Expires = DateTime.Now.AddDays(-1);
+            }
+            Session.Abandon();
+
+            return RedirectToAction("Index");
         }
     }
 }
